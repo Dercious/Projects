6c149db54b3341ffc7d60e14da10fa449c26fdd4
Dercious <119151460+Dercious@users.noreply.github.com>
2023-02-19 16:16:10 +0530

    Add files via upload
    updated

--- 1.Date-Time/script.js
+++ 1.Date-Time/script.js
@@ -4,7 +4,6 @@ var TodayDate = document.getElementById('date');
 var TodayDay = document.getElementById('day');
 var TodayTime = document.getElementById('time');
 var noofdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
 function CurrentDate() {
     var day = date.getDate();
     var month = date.getMonth() + 1;
@@ -20,19 +19,31 @@ function CurrentDay() {
 CurrentDay();
 
 function CurrentTime() {
+    var date=new Date();
     var hour = date.getHours();
     var min = date.getMinutes();
     var sec = date.getSeconds();
+    var ampm = "AM"
 
-    // if(hour>12){
-    //     hour=hour-12;
-    // }
+    if(hour==0){
+        hour=12
+    }
+    if (hour>12)
+    {
+        hour=hour-12
+        ampm="PM"
+    }
+    
+    hour=hour<10?"0"+hour:hour;
+    min=min<10?"0"+min:min;
+    sec=sec<10?"0"+sec:sec;
 
-    TodayTime.innerHTML = hour+" Hours "
-    + min +" Mins "
-    + sec +" Sec ";
+    TodayTime.innerHTML = hour+": "
+    + min +": "
+    + sec + " "+ampm;
     setTimeout(()=>{
         CurrentTime()
     },1000)
-}console.log(CurrentTime());
+}
 CurrentTime();
+
